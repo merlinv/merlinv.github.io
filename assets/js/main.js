0f0c10ecb1485e1b885e1f2e1e722ac34b7e91c7
duykhanh <summondk@gmail.com>
2014-04-01 22:25:09 +0700

    Add assest files

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+/*
+  Author: CDK
+  Version: 1.0.0
+*/
+console.log("Game Loading...");
+var SETTINGS = {
+  GRID_SIZE: 6,
+  STATEs: 2
+};
+var CONSTANTS = {
+  item_class: "lighter-item",
+  game_box_class: "game-box"
+};
+var LIGHTER = {
+  init: function(){
+
+  },
+  finish: function(){
+
+  }
+};
