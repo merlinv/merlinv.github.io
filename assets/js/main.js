1b27d34e7c1789635f3d5da1db169d126df79b25
Duy Khanh <khanhcd2110@gmail.com>
2014-04-07 08:56:30 +0700

    Release v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,14 +12,15 @@ var SETTINGS = {
   COLOR: {
     on:  "#D31717",
     off: "#fffaf0"
-  }
+  },
+  MOVE: 0
 };
 var STUFF = {
   item: function(){
     var item = document.createElement("DIV");
     item.className = "lighter-item";
     item.state = "off";
-    item.style.backgroundColor = SETTINGS.STATES.off; 
+    item.style.backgroundColor = SETTINGS.STATES.off;
     return item;
   },
   row: function(longitude){
@@ -38,6 +39,9 @@ var STUFF = {
 };
 var LIGHTER = {
   pool: [],
+  updateMove: function(){
+    $("#your-move").text(SETTINGS.MOVE); //update move
+  },
   getLighterByPos: function(y,x){
     if (this.pool[y]){
       return this.pool[y][x]
@@ -52,46 +56,72 @@ var LIGHTER = {
   },
   getLandOfLighter: function(lighter){
     var pos = this.getPosOfLighter(lighter);
-    return [ [pos.x, pos.y + 1], [pos.x+1, pos.y+1], [pos.x+1, pos.y], [pos.x+1, pos.y-1], [pos.x, pos.y-1], [pos.x-1, pos.y-1], [pos.x-1, pos.y], [pos.x-1, pos.y+1] ]; 
+    return [ [pos.x, pos.y + 1], [pos.x+1, pos.y+1], [pos.x+1, pos.y], [pos.x+1, pos.y-1], [pos.x, pos.y-1], [pos.x-1, pos.y-1], [pos.x-1, pos.y], [pos.x-1, pos.y+1] ];
   },
   init: function(){
+    var that = this;
+    SETTINGS.MOVE = 0;
+    this.updateMove();
     var grid = document.getElementsByClassName("grid-box")[0];
     for (var j = 0; j < SETTINGS.GRID_SIZE; j++){
       var row = STUFF.row(j);
       this.pool.push(row.items);
       grid.appendChild(row);
     }
+    $(".lighter-item").click(function(e){
+      SETTINGS.MOVE += 1;
+      that.updateMove();
+      var land = LIGHTER.getLandOfLighter(this);
+      $.each(land, function(_, pos){
+        var lighter = LIGHTER.getLighterByPos(pos[1], pos[0]);
+        if (lighter != undefined){
+          lighter.state = SETTINGS.STATES[lighter.state];
+          $(lighter).flip({
+            direction: "lr",
+            color: SETTINGS.COLOR[lighter.state],
+            speed: "fast"
+          })
+        }
+      });
+      setTimeout(function(){LIGHTER.checkGameFinished()}, 500);
+      e.preventDefault();
+    })
   },
   finish: function(){
+    var that = this;
+    $('#my-modal').modal('show');
+    $('#score').text(SETTINGS.MOVE);
+    $('.grid-box').click(function(e){
+      e.preventDefault();
+    })
+    $('#my-modal').on('hidden.bs.modal', function () {
+      that.reload();
+    })
+  },
+  reload: function(){
+    $(".grid-box").empty();
+    this.pool = [];
+    this.init();
+  },
+  checkGameFinished: function(){
+    var check = true;
+    var pool = this.pool;
+    $.each(pool, function(_, row){
+      $.each(row, function(_, lighter){
+        if (lighter.state == "off"){
+          check = false;
+          return;
+        }
+      })
+    })
+    if (check){
+      this.finish();
+    }
   }
 };
 window.onload = function(){
   LIGHTER.init();
-  $(".lighter-item").click(function(e){
-    // $(this).flip({
-    //   direction: "lr",
-    //   color: "#D31717",
-    //   speed: "fast",
-    //   onAnimation: function(){
-
-    //   },
-    //   onEnd: function(){
-
-    //   }
-    // });
-    var land = LIGHTER.getLandOfLighter(this);
-    $.each(land, function(_, pos){
-      var lighter = LIGHTER.getLighterByPos(pos[1], pos[0]);
-      if (lighter != undefined){
-        lighter.state = SETTINGS.STATES[lighter.state];
-        $(lighter).flip({
-          direction: "lr",
-          color: SETTINGS.COLOR[lighter.state],
-          speed: "fast"
-        })
-      }
-      
-    })
-    e.preventDefault(); 
+  $("#replay").click(function(){
+    LIGHTER.reload();
   })
 }
